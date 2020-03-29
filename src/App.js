daab3be35b25cfe22ad363036e6780a86c8a7c99
clheard54 <clarion.heard@gmail.com>
2020-03-29 14:53:19 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import BlogPost from './BlogPost.js'
 import ColorBox from './ColorBox.js'
+import CommentComponent from './Comment.js'
 
 class App extends Component {
   
@@ -13,7 +14,7 @@ class App extends Component {
           <BlogPost />
           <div id="seperator"></div>
           <div className="wrapper">
-            <ColorBox opacity={1} />
+            <ColorBox opacity={1.0} />
           </div>
       </div>
     )
